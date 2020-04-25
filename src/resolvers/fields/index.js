324366e2056cac71f4bfb6bac11fb93ec41ad235
Amir Ali <amir.saeed.ali92@gmail.com>
2020-04-25 22:23:34 +0500

    setting up testing envirnment

--- src/resolvers/fields/index.js
+++ src/resolvers/fields/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     User: {
         heroes: async (parent, _, {User}, ___) => {
-            const user = await User.findById(parent.id);
+            const user = await User.findById(parent.id || parent._id);
 
             if(user) {
                 await user.populate('heroes').execPopulate();
--- src/resolvers/mutations/mutations.test.js
+++ src/resolvers/mutations/mutations.test.js
@@ -1,6 +1,41 @@
-describe("mutations tests", () => {
-    test("initial test", () => {
-        expect(1).toBe(1);
+const mongoose = require('mongoose');
+const {gql} = require('apollo-server-express');
+const CreateTestServer = require('../../test-server');
+const {User, Hero} = require('../../mongodb/models');
+
+const CREATE_USER = gql`
+    mutation {
+        createUser(input: {name: "test", email: "test@test.com", password: "123456"}) {
+            name
+            email
+            password
+        }
+    }
+`;
+
+
+describe("user mutation tests", () => {
+
+    beforeAll(async () => {
+        await mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false}, (err) => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+        });
+    });
+
+    test("it should create a user in db", async () => {
+
+        const {mutate} = CreateTestServer({
+            User,
+            Hero
+        })
+
+        const res = await mutate({mutation: CREATE_USER});
+
+        expect(res).toMatchSnapshot();
+        expect(1).toBeTruthy();
     });
 });
 
